8fbc2050c35acc78566f10cc13fdeb02c0eeeb25
Rogério Teixeira <rogerio.teixeira@inspira.com.br>
2018-10-13 13:27:57 -0300

    Adicionado código fix para bloco de código error

--- project/rules/http-deprecated.js
+++ project/rules/http-deprecated.js
@@ -29,17 +29,21 @@ module.exports = {
                 if (node.callee.property.name === 'success' && isHttpCall(node)) {
                     context.report({
                         node: node,
-                        message: "'$http {identifier} is deprecated.",
-                        data: {
-                            identifier: node.callee.property.name
-                        },
+                        message: "'$http success is deprecated.",
                         fix: function (fixer) {
                                return fixer.replaceText(node.callee.property, 'then');
                         }
                     })
                 }
                 if (node.callee.property.name === 'error' && isHttpCall(node)) {
-                    context.report(node, '$http error is deprecated. Use then or catch instead');
+                    context.report({
+                        node: node,
+                        message: "'$http error is deprecated.",
+                        fix: function(fixer){
+                            var rangeToReplace = [node.callee.property.range[0] -2, node.callee.property.range[1] + 1];
+                            return fixer.replaceTextRange(rangeToReplace, ',');
+                        }
+                    })
                 }
             }
         };
